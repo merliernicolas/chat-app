d0cd89a5085eed42643e358dae78e564de390ef0
secoueur <54461651+secoueur@users.noreply.github.com>
2019-10-09 11:26:26 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Contact from './components/Contact';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Contact
+        name="Salibor"
+        avatar="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Ancylostoma_duodenale_boca.jpg/640px-Ancylostoma_duodenale_boca.jpg"
+        online
+      />
+      <Contact
+        name="DKAnky"
+        avatar="https://img.point.pet/images/hook-worm-artwork-460715065-584dd9f05f9b58a8cdebf117.jpg"
+        online
+      />
+      <Contact
+        name="Galactos"
+        avatar="https://staticserver2.com/edu/static/fr/800/ancylostoma-duodenale.jpg"
+        offline
+      />
     </div>
   );
 }
