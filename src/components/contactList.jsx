a0e2261c1e1cdd79eedc884a360ba3fa1f5adfa5
Nicolas Merlier <nicolasmerlier@gmail.com>
2019-10-14 10:03:27 +0200

    realisation de la quete React 3

---
+++ src/components/contactList.jsx
@@ -0,0 +1,25 @@
+const contactList = [
+    {
+      name: 'Tom Ambrose',
+      avatar: 'https://randomuser.me/api/portraits/men/32.jpg'
+    },
+    {
+      name: 'Rick Johansson',
+      avatar: 'https://randomuser.me/api/portraits/men/14.jpg'
+    },
+    {
+      name: 'Viviane Hannessy',
+      avatar: 'https://randomuser.me/api/portraits/women/56.jpg' 
+    },
+    {
+      name: 'Clay Jordan',
+      avatar: 'https://randomuser.me/api/portraits/men/88.jpg' 
+    },
+    {
+      name: 'Nathan Schnitt',
+      avatar: 'https://randomuser.me/api/portraits/men/64.jpg'
+    }
+  ];
+
+
+  export default contactList;
